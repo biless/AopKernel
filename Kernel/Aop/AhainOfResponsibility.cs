13c78769fbf5bae59b697a046589ca95fb3f0e7f
kzp <kzp@qq.com>
2015-10-22 13:36:33 +0800

    增加责任链模式 用做支持多种不同切面 类型 处理

---
+++ Kernel/Aop/AhainOfResponsibility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kernel.Aop
+{
+    /// <summary>
+    /// 责任连锁
+    /// </summary>
+    class AhainOfResponsibility
+    {
+        private readonly AhainOfResponsibility ahainOfResponsibility = null;
+
+        public Action<AhainOfResponsibility> exectueNext;
+
+        public AhainOfResponsibility()
+        { }
+
+        public AhainOfResponsibility(AhainOfResponsibility ahainOfResponsibility)
+        {
+            this.ahainOfResponsibility = ahainOfResponsibility;
+        }
+
+        public void exectue()
+        {
+            exectueNext?.Invoke(ahainOfResponsibility);
+        }
+    }
+}
--- Kernel/Aop/MethodInterceptor.cs
+++ Kernel/Aop/MethodInterceptor.cs
@@ -10,17 +10,17 @@ namespace Kernel.Aop
 {
     class MethodInterceptor : IInterceptor
     {
+        private IInvocation invocation;
+
         public void Intercept(IInvocation invocation)
         {
-            var att = invocation.Method.GetCustomAttributes(typeof(AsepctAttribute), true).Cast<AsepctAttribute>().ToList();
-            attributeBeforeAsepct(att);
-            invocation.Proceed();
-            var task = invocation.ReturnValue as Task;
+            this.invocation = invocation;
 
-            if (task == null)
-                attributeAfterAsepct(att);
-            else
-                attributeAfterAsepctTask(task,att);
+            var ahainOfResponsibility = new AhainOfResponsibility {exectueNext = x => invocation.Proceed()};
+
+            ahainOfResponsibility = new AhainOfResponsibility(ahainOfResponsibility) {exectueNext = asepct};
+
+            ahainOfResponsibility.exectue();
 
             if (invocation.Method.Name.StartsWith("set_"))
             {
@@ -29,6 +29,31 @@ namespace Kernel.Aop
                 notifyPropertyChangedObject?.onPropertyChanged(invocation.Method.Name.Substring(4));
             }
         }
+
+        private void tryCatchAsepct(AhainOfResponsibility ahainOfResponsibility)
+        {
+
+        }
+
+        private void argumentJudgementAsepct(AhainOfResponsibility ahainOfResponsibility)
+        {
+
+        }
+
+        private void asepct(AhainOfResponsibility ahainOfResponsibility)
+        {
+            var att = invocation.Method.GetCustomAttributes(typeof(AsepctAttribute), true).Cast<AsepctAttribute>().ToList();
+            attributeBeforeAsepct(att);
+
+            ahainOfResponsibility.exectue();
+
+            var task = invocation.ReturnValue as Task;
+            if (task == null)
+                attributeAfterAsepct(att);
+            else
+                attributeAfterAsepctTask(task, att);
+        }
+
 
         private void attributeBeforeAsepct(List<AsepctAttribute> asepctAttributes)
         {
@@ -39,7 +64,7 @@ namespace Kernel.Aop
             });
         }
 
-        private void attributeAfterAsepctTask(Task task,List<AsepctAttribute> asepctAttributes)
+        private void attributeAfterAsepctTask(Task task, List<AsepctAttribute> asepctAttributes)
         {
             task.ContinueWith(x => attributeAfterAsepct(asepctAttributes));
         }
